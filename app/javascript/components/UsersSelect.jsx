3f3158ddd029b5f414ca7d0b5c8c9d3798aec481
Shoaib Iqbal <shoaib.iqbal07@gmail.com>
2022-10-19 22:21:11 +0500

    Removed test code

--- app/javascript/components/UsersSelect.jsx
+++ app/javascript/components/UsersSelect.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Select from 'react-select'
-const options1 = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-]
 
 class UsersSelect extends React.Component {
     constructor(props) {
